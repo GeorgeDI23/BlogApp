8334f6f614dbb51f17c2aed07b84997caf8657e5
George Ionas <gionas@zipcoders-MacBook-Pro.local>
2020-08-31 15:01:31 -0400

    spring setup

---
+++ gjblog/src/main/java/com/zipcode/gjblog/GjblogApplication.java
@@ -0,0 +1,13 @@
+package com.zipcode.gjblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjblogApplication.class, args);
+	}
+
+}
---
+++ gjblog/src/test/java/com/zipcode/gjblog/GjblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcode.gjblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
